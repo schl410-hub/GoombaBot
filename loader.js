196a763109f9d8fef7ae820110a8f927b445c04b
schl410-hub <schl410@gmail.com>
2026-07-24 22:48:01 +0900

    Update loader.js

--- loader.js
+++ loader.js
@@ -25,7 +25,7 @@
 
 // TODO: 실제 GitHub raw main.js 주소로 교체해주세요.
 // 예: "https://raw.githubusercontent.com/사용자명/저장소명/main/main.js"
-var GOOMBABOT_MAIN_JS_URL = "";
+var GOOMBABOT_MAIN_JS_URL = "https://raw.githubusercontent.com/schl410-hub/GoombaBot/main/main.js";
 
 var GoombaBotRuntime = {};
 
